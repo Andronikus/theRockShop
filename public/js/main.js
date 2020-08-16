64d0050bc2e6caa19eb9214b15b19e9161addc85
Andronikus <tiagoafroque@gmail.com>
2020-08-16 22:51:29 +0100

    improve orders page

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,37 @@
-const backdrop = document.querySelector('.backdrop');
-const sideDrawer = document.querySelector('.mobile-nav');
-const menuToggle = document.querySelector('#side-menu-toggle');
+// const backdrop = document.querySelector(".backdrop");
+// const sideDrawer = document.querySelector(".mobile-nav");
+// const menuToggle = document.querySelector("#side-menu-toggle");
 
-function backdropClickHandler() {
-  backdrop.style.display = 'none';
-  sideDrawer.classList.remove('open');
+// function backdropClickHandler() {
+//   backdrop.style.display = "none";
+//   sideDrawer.classList.remove("open");
+// }
+
+// function menuToggleClickHandler() {
+//   backdrop.style.display = "block";
+//   sideDrawer.classList.add("open");
+// }
+
+// backdrop.addEventListener("click", backdropClickHandler);
+// menuToggle.addEventListener("click", menuToggleClickHandler);
+
+const toogleInfo = {};
+
+function toogleDetails(e) {
+  console.log(e.target.parentElement.parentElement.children[1]);
+  const orderContainer = e.target.parentElement.parentElement;
+  const orderDetails = orderContainer.children[1];
+  const orderContainerId = orderContainer.id;
+
+  if (!toogleInfo[orderContainerId]) {
+    orderDetails.classList.remove("hide");
+    orderDetails.classList.add("show");
+    e.target.textContent = "less details";
+    toogleInfo[orderContainerId] = true;
+  } else {
+    orderDetails.classList.remove("show");
+    orderDetails.classList.add("hide");
+    e.target.textContent = "more details";
+    delete toogleInfo[orderContainerId];
+  }
 }
-
-function menuToggleClickHandler() {
-  backdrop.style.display = 'block';
-  sideDrawer.classList.add('open');
-}
-
-backdrop.addEventListener('click', backdropClickHandler);
-menuToggle.addEventListener('click', menuToggleClickHandler);
