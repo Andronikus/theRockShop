8cbbf6de8d0060288ba83141a0d2d419e5ecbd15
Andronikus <tiagoafroque@gmail.com>
2020-08-22 16:21:51 +0100

    add isAuthenticated middleware function + protect all routes that need logged in info

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,26 @@
 const express = require("express");
 
 const adminController = require("../controllers/admin");
+const { isAuthenticated } = require("../middleware/isAuthenticated");
 
 const route = express.Router();
 
-route.get("/add-product", adminController.getAddProduct);
-route.post("/add-product", adminController.postAddProduct);
+route.get("/add-product", isAuthenticated, adminController.getAddProduct);
+route.post("/add-product", isAuthenticated, adminController.postAddProduct);
 
-route.get("/edit-product/:productId", adminController.getEditProduct);
-route.post("/edit-product", adminController.postEditProduct);
+route.get(
+  "/edit-product/:productId",
+  isAuthenticated,
+  adminController.getEditProduct
+);
+route.post("/edit-product", isAuthenticated, adminController.postEditProduct);
 
-route.post("/delete-product", adminController.postDeleteProduct);
+route.post(
+  "/delete-product",
+  isAuthenticated,
+  adminController.postDeleteProduct
+);
 
-route.get("/list-products", adminController.getProducts);
+route.get("/list-products", isAuthenticated, adminController.getProducts);
 
 module.exports = route;
--- routes/shop.js
+++ routes/shop.js
@@ -1,16 +1,21 @@
 const express = require("express");
 
 const shopController = require("../controllers/shop");
+const { isAuthenticated } = require("../middleware/isAuthenticated");
 
 const route = express.Router();
 
 route.get("/", shopController.getIndex);
 route.get("/products", shopController.getProducts);
 route.get("/products/:productId", shopController.getProduct);
-route.get("/cart", shopController.getCart);
-route.post("/cart", shopController.postAddProductToCart);
-route.post("/cart/delete-product", shopController.postRemoveProductFromCart);
-route.post("/create-order", shopController.postCreateOrder);
-route.get("/orders", shopController.getOrders);
+route.get("/cart", isAuthenticated, shopController.getCart);
+route.post("/cart", isAuthenticated, shopController.postAddProductToCart);
+route.post(
+  "/cart/delete-product",
+  isAuthenticated,
+  shopController.postRemoveProductFromCart
+);
+route.post("/create-order", isAuthenticated, shopController.postCreateOrder);
+route.get("/orders", isAuthenticated, shopController.getOrders);
 
 module.exports = route;
